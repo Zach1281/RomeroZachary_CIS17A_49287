56d6ad579212ead8f10eddae19493545f94c2f9f
Zach1281 <zachary_romero@yahoo.com>
2020-08-25 15:08:17 -0700

    IntroLab

---
+++ Lab/IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+/* 
+ * File:   HelloWorld.cpp
+ * Author: Zachary Romero
+ * Created on August 25, 2020, 2:53 PM
+ */
+
+#include <iostream>
+
+int main ()
+{
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
